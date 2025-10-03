e2fd09af7851ec1c78afdbf57315f46d5a923ae8
araunabagas <araunamanroe@gmail.com>
2025-10-03 20:43:33 +0700

    Pair3
    Sukses

--- T00.java
+++ T00.java
@@ -33,8 +33,8 @@ public class T00 {
             c = a + b;
             System.out.println("Hasil penjumlahan: " + c);
         } else {
-            if (b + 1 % 2 == 0 && a % 2 == 0) {
-                d = a * b;
+            if (a % 2 != 0 && b % 2 != 0) {
+                c = a * b;
                 System.out.println("Hasil perkalian: " + c);
             } else {
                 System.out.println("Berbeda jenis");
